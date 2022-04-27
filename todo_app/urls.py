96ab5930c5f7615238d7e030ae2559a6d7e874aa
wchriestenson <wadechriestenson@gmail.com>
2022-04-27 11:41:55 -0700

    Initial commit 04/27/2022 - WC

---
+++ todo_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from todo_app import views
+
+urlpatterns = [
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/",
+         views.ItemListView.as_view(), name="list"),
+]
---
+++ todo_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoList
+from .models import ToDoList, ToDoItem
+
+
+# Create your views here.
+class ListListView(ListView):
+    model = ToDoList
+    template_name = 'index.html'
+
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo_list.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
